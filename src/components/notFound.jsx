865a868ce8ead3fdba65e9838edeeee281a33eb5
Volodia Choriy <36338478+VolodiaCH@users.noreply.github.com>
2020-08-27 10:43:43 +0300

    Update notFound.jsx

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -8,6 +8,7 @@ class NotFound extends Component {
                 <span style={{ fontSize: "100px" }}>404</span>
                 <h3>Page you are looking for does not exist</h3>
                 <br />
+                <h4>Maybe user profile has been deleted or username changed.</h4>
                 <button type="button" className="btn btn-primary" onClick={() => window.location = "/home"}>Go home</button>
             </div>
         );
